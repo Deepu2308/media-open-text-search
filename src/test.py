39440d61632fc000cec2b9e1647dab64d476cc15
Deepu Unnikrishnan <deeputvm1992@gmail.com>
2023-12-09 19:14:49 +0530

    version printer

--- src/test.py
+++ src/test.py
@@ -1,14 +1,12 @@
 import torch
 import pandas as pd
-from transformers import pipeline
+import transformers
 
 def main():
     print("PyTorch version:", torch.__version__)
     print("Pandas version:", pd.__version__)
+    print("HuggingFace Version:" , transformer.__version__)
 
-    unmasker = pipeline('fill-mask', model='bert-base-uncased')
-    output = unmasker("The installation worked and i feel [MASK] about it!!")[0]['sequence'].upper()
-    print(output)
 
 if __name__ == "__main__":
     main()
