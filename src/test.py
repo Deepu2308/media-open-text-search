b95e1e92377f96a69c07adde5466be495622618a
Deepu Unnikrishnan <deeputvm1992@gmail.com>
2023-12-09 19:16:05 +0530

    bug fix

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ import transformers
 def main():
     print("PyTorch version:", torch.__version__)
     print("Pandas version:", pd.__version__)
-    print("HuggingFace Version:" , transformer.__version__)
+    print("HuggingFace Version:" , transformers.__version__)
 
 
 if __name__ == "__main__":
